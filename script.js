0286423b7278209fe026bbd33c578c72942d0318
idabaicaiqz <depwsh74lkni@sina.com>
2020-12-30 15:32:30 +0800

    converted Date to Date in string
    if day is 5 then ---> it will become "05"
    same for month

--- script.js
+++ script.js
@@ -1,13 +1,36 @@
-function reverse1(string){
+function reverse(string){
     var splitCharacters = string.split("");
     var reversedCharacters = splitCharacters.reverse();
     var joinedChar = reversedCharacters.join("");
     return joinedChar;
-    // return string.split("").reverse().join("");
+    // return srting2.split("").reverse().join(""); ----same as above;
+   
 }
-console.log(reverse1("manthan"));
-function reverse2(srting2){
-    return srting2.split("").reverse().join("");
+function isPalindrome(string){
+   var reverse1 =  reverse(string);
+   return string ===reverse1;
 }
-console.log(reverse2("manthan"))
-
+function dateToString(date){
+    var datestr = { day : "",month : "", year : ""}
+     if(date.day < 10 ){
+         datestr.day = "0"+date.day;
+     }
+     else{
+         datestr.day=date.day.toString();
+     }
+     if(date.month < 10 ){
+        datestr.month = "0"+date.month;
+    }
+    else{
+        datestr.month=date.month.toString();
+    }
+    datestr.year = date.year.toString();
+    return datestr;
+}
+var date = {
+    day:15,
+    month:12,
+    year:2000
+}
+console.log(date);
+console.log(dateToString(date));
